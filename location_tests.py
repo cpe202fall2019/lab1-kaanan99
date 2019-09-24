84d55952393a2a45031e1737deda11b0359e39ef
Kaanan Kharwa <kaanan99@gmail.com>
2019-09-23 23:40:58 -0700

    Added comments and more cases

--- location_tests.py
+++ location_tests.py
@@ -4,29 +4,43 @@ from location import *
 class TestLab1(unittest.TestCase):
 
     def test_repr(self):
+        #Making the default object
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        #Creating a different object
         loc2 = Location("Paris", 48.9, 2.4)
         self.assertEqual(repr(loc2), "Location('Paris', 48.9, 2.4)")
     def test_eq(self):
         loc1 = Location("SLO", 35.3, -120.7)
         loc2 = Location("Paris", 48.9, 2.4)
         loc3 = loc1
-        loc4 = Location("SLO", 35.3, -120.7)
+        loc4 = Location("SLO", 35.3, -120.70000001)
+        loc5 = Location("SLO", 35.3, -120.7)
+        #Testing to see if two different objects are equal
         self.assertFalse(loc1 == loc2)
-        self.assertTrue(loc1 == loc4)
+        #Testing to see if a refrence to the same object is equal
         self.assertTrue(loc1 == loc3)
-        self.assertFalse(loc2 == loc3)
+        #Testing to see if two identical objects are equal
+        self.assertTrue(loc1 == loc5)
+        #Testing for different types
+        self.assertFalse(loc1 == "hi")
+        #Testing for rounding
+        self.assertTrue(loc1 == loc4)
+        
+        
     def test_init(self):
         loc1 = Location("SLO", 35.3, -120.7)
         loc2 = Location("Paris", 48.9, 2.4)
         loc3 = loc1
         loc4 = Location("SLO", 35.3, -120.7)
+        #To see if two different objects are the same
         self.assertIsNot(loc1, loc4)
+        #To see if a refrence to same object is the same
         self.assertIs(loc1, loc3)
+        #To see if different objects with different info are the same
         self.assertIsNot(loc1, loc2)
+        #To see if object and refrence to difference object are the same
         self.assertIsNot(loc2, loc3)
-        self.assertIsNot(loc4, loc2)
     # Add more tests!
 
 if __name__ == "__main__":
